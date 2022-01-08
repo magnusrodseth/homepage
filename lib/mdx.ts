77fb8579546c2ec40a965b08f1d45b24644bfc1b
magnusrodseth <59113973+magnusrodseth@users.noreply.github.com>
2022-01-08 22:20:29 +0100

    Add proper README, and add documentation in mdx.ts

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -17,11 +17,23 @@ export const MDX_ABOUT_PATH = 'content/about'
 
 export type MDXDirectory = 'content/blog' | 'content/projects' | 'content/about'
 
+/**
+ * Gets .mdx content based on provided slug and directory.
+ * @param slug is the slug corresponding to the .mdx file. For instance, the file 'hello-word.mdx' gets the slug 'hello-word'.
+ * @param directory is the directory of the .mdx content.
+ * @returns the frontmatter and markdown content from the .mdx file.
+ */
 export const getMdxBySlug = async (slug: string, directory: MDXDirectory) => {
     return getMdxByPath(path.join(directory, `${slug}.mdx`))
 }
 
-export const getMdxByPath = async (mdxPath: string) => {
+/**
+ * Helper method for the function `getMdxBySlug`. 
+ * Gets .mdx content based on provided slug and directory.
+ * @param mdxPath is the full path to the desired .mdx file.
+ * @returns the frontmatter and markdown content from the .mdx file.
+ */
+const getMdxByPath = async (mdxPath: string) => {
     const slug = path.basename(mdxPath).replace(path.extname(mdxPath), '')
     const source = fs.readFileSync(path.join(process.cwd(), mdxPath), 'utf8')
     const { code, frontmatter } = await bundleMDX(source, {
@@ -47,6 +59,11 @@ export const getMdxByPath = async (mdxPath: string) => {
     }
 }
 
+/**
+ * Gets all frontmatters for a given directory.
+ * @param directory is the directory with .mdx content.
+ * @returns all frontmatters for a given directory.
+ */
 export const getFrontMatters = async (directory: MDXDirectory): Promise<FrontMatter[]> => {
     const paths = await globby([`${directory}/**/*.mdx`])
     const matters = await Promise.all(
