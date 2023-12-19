7140615c01e58ececf6a8fa5b53e0c7799f1ab88
magnusrodseth <magnus.rodseth@gmail.com>
2023-12-19 14:40:28 +0100

    update back link and blog content

--- src/components/back-link.tsx
+++ src/components/back-link.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { Icons } from "./icons";
 import { Button } from "./ui/button";
 
 const BackLink = () => {
   const router = useRouter();
+  const pathname = usePathname();
+  const childPage = pathname.split("/")[1] ?? undefined;
 
   return (
     <Button
       className="flex justify-center items-center gap-x-2 text-muted-foreground my-8 px-0 animate-slide-enter"
       variant="link"
       size="sm"
-      onClick={() => router.back()}
+      onClick={() => {
+        if (childPage) {
+          router.push(`/${childPage}`);
+        } else {
+          router.push("/");
+        }
+      }}
     >
       <Icons.chevronRight className="h-5 w-5" />
       <span className="font-mono text-lg">cd ..</span>
